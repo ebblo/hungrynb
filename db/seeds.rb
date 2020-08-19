e69dae51e0393fb17435f9e4ae64373dcaa61624
Johann Groll <groll.johann@gmail.com>
2020-08-19 14:23:51 +0200

    change pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,9 @@
 require "faker"
 require "open-uri"
 
-# file_1 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830555/dose-juice-sTPy-oeA3h0-unsplash_uf1pul.jpg")
-# file_2 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830555/thomas-tucker-MNtag_eXMKw-unsplash_r3gk8x.jpg")
-# file_3 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830555/dan-gold-4_jhDO54BYg-unsplash_are4nk.jpg")
-# file_4 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830554/joseph-gonzalez-fdlZBWIP0aM-unsplash_b72b1c.jpg")
-# file_5 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830554/brooke-lark-08bOYnH_r_E-unsplash_kgrhle.jpg")
-# file_6 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830554/lily-banse--YHSwy6uqvk-unsplash_stknqd.jpg")
-# file_7 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597830553/eiliv-sonas-aceron-ZuIDLSz3XLg-unsplash_bgpxxp.jpg")
-# file_8 = URI.open("https://res.cloudinary.com/dsszx2brq/image/upload/v1597829469/zdnty8c3nfcc9sh8dwdvae56zb3t.jpg")
-# pictures = [file_1, file_2, file_3, file_4, file_5, file_6, file_7, file_8]
 
-file = URI.open("https://source.unsplash.com/collection/251966")
+
+# file = URI.open("https://source.unsplash.com/collection/251966")
 
 puts "Deleting old seeds"
 Booking.destroy_all
@@ -42,7 +34,7 @@ puts "Start seeding"
     description: "miam c'est bon", 
     date: "2020/05/16"
   )
-  my_offer.photo.attach(io: file, filename: "picture.png", content_type: "image/png")
+  # my_offer.photo.attach(io: pictures.sample, filename: "picture.png", content_type: "image/jpeg")
   my_offer.user = my_user
   my_offer.save!
 end
