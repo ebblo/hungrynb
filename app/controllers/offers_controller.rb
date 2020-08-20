f5c95ba24674142bd1ef4f2d7d29a03dafcee598
Johann Groll <groll.johann@gmail.com>
2020-08-20 16:22:37 +0200

    bugfix

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,15 +2,10 @@ class OffersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_offer, only: [:show, :edit, :update, :destroy]
   
-  def index
-    
+  def index    
     if params[:query].present?
       sql_query = "users.address ILIKE :query AND offers.date >= :start_date AND offers.date <= :end_date"
       @offers = Offer.joins(:user).where(sql_query, query: "%#{params[:query]}%", start_date: params[:start_date], end_date: params[:end_date])
-      
-      # @users_selected = User.joins(:offer).where(sql_query, query: "%#{params[:query]}%", start_date: params[:start_date], end_date: params[:end_date])
-      # @users = @users_selected.geocoded # returns users with coordinates
-
       @markers = @offers.map do |offer|
         {
           lat: offer.user.latitude,
@@ -22,18 +17,6 @@ class OffersController < ApplicationController
     else
       @offers = Offer.all
     end
-      
-
-    # @users = User.geocoded # returns users with coordinates
-
-    # @markers = @users.map do |user|
-    #   {
-    #     lat: user.latitude,
-    #     lng: user.longitude,
-    #     infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
-    #     image_url: helpers.asset_url('/Users/loicebbing/code/ebblo/rbnb/app/assets/images/pasta.jpg')
-    #   }
-    # end
   end
 
   def show
